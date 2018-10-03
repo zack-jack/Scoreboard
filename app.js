4e5b97d40ae48ab47efee12f9c406e263ef0b369
zackjacksongit <z.jackson58@gmail.com>
2018-10-03 14:03:45 -0400

    Increment and Decrement handling onClick

--- app.js
+++ app.js
@@ -28,12 +28,34 @@ class Counter extends React.Component {
     score: 0
   };
 
+  incrementScore = () => {
+    this.setState({
+      score: this.state.score + 1
+    });
+  };
+
+  decrementScore = () => {
+    this.setState({
+      score: this.state.score - 1
+    });
+  };
+
   render() {
     return (
       <div className="counter">
-        <button className="counter-action decrement"> - </button>
+        <button
+          className="counter-action decrement"
+          onClick={this.decrementScore}
+        >
+          -
+        </button>
         <span className="counter-score">{this.state.score}</span>
-        <button className="counter-action increment"> + </button>
+        <button
+          className="counter-action increment"
+          onClick={this.incrementScore}
+        >
+          +
+        </button>
       </div>
     );
   }
