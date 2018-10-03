6108bff8749afe1e99b61768516c698b1bae1c8d
zackjacksongit <z.jackson58@gmail.com>
2018-10-03 16:27:25 -0400

    Components broken out into individual files

---
+++ src/components/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+
+import Header from "./Header";
+import Player from "./Player";
+
+class App extends Component {
+  state = {
+    players: [
+      {
+        name: "Player 1",
+        id: 1
+      },
+      {
+        name: "Player 2",
+        id: 2
+      },
+      {
+        name: "Player 3",
+        id: 3
+      }
+    ]
+  };
+
+  handleRemovePlayer = id => {
+    this.setState(prevState => {
+      return {
+        players: prevState.players.filter(p => p.id !== id)
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div className="scoreboard">
+        <Header title="Scoreboard" totalPlayers={this.state.players.length} />
+
+        {/* Players list */}
+        {this.state.players.map(player => (
+          <Player
+            name={player.name}
+            id={player.id}
+            key={player.id.toString()}
+            removePlayer={this.handleRemovePlayer}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default App;
