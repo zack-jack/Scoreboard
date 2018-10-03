c9820629fdf81565788d0b0dfacb12b747f04465
zackjacksongit <z.jackson58@gmail.com>
2018-10-03 10:48:25 -0400

    Counter, Player and App components

--- app.js
+++ app.js
@@ -5,4 +5,26 @@ const Header = () => (
   </header>
 );
 
-ReactDOM.render(<Header />, document.getElementById("root"));
+const Counter = () => (
+  <div className="counter">
+    <button className="counter-action decrement"> - </button>
+    <span className="counter-score">35</span>
+    <button className="counter-action increment"> + </button>
+  </div>
+);
+
+const Player = () => (
+  <div className="player">
+    <span className="player-name">Zack</span>
+    <Counter />
+  </div>
+);
+
+const App = () => (
+  <div className="scoreboard">
+    <Header />
+    <Player />
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
