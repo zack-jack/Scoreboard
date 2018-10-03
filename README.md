52b8f8f884ae5dbb48a685968e8aa2d12e4aa9e7
zackjacksongit <z.jackson58@gmail.com>
2018-10-03 10:35:12 -0400

    Readme.MD

---
+++ README.md
@@ -0,0 +1,3 @@
+# Scoreboard React App
+
+Project code for the [Treehouse](https://teamtreehouse.com) courses [React Basics & React Components](https://teamtreehouse.com/tracks/learn-react)
---
+++ app.js
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header>
+      <h1>Scoreboard</h1>
+      <span className="stats">Players: 1</span>
+    </header>
+  );
+}
+
+ReactDOM.render(header, document.getElementById("root"));
