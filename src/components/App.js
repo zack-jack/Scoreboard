06ac044cd2c47db5a49ee64026410f38af7fb020
zackjacksongit <z.jackson58@gmail.com>
2018-10-05 20:58:16 -0400

    handleAddPlayer refactor with prevState

--- src/components/App.js
+++ src/components/App.js
@@ -34,15 +34,17 @@ class App extends Component {
   };
 
   handleAddPlayer = name => {
-    this.setState({
-      players: [
-        ...this.state.players,
-        {
-          name,
-          score: 0,
-          id: (this.prevPlayerId += 1)
-        }
-      ]
+    this.setState(prevState => {
+      return {
+        players: [
+          ...prevState.players,
+          {
+            name,
+            score: 0,
+            id: (this.prevPlayerId += 1)
+          }
+        ]
+      };
     });
   };
 
